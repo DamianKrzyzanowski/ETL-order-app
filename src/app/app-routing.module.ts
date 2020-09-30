1abb9b5ed7923509f9b3336550b3c6891bf88479
Damian Krzyzanowski <damian.krzyzanowski1994@gmail.com>
2020-09-30 20:03:57 +0200

    Updated Basic Version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { RequestFormComponent } from './request-form/request-form.component';
+import { OrdersListComponent } from './orders-list/orders-list.component';
+import { SqlOrderFormComponent } from './sql-order-form/sql-order-form.component';
+import { CsvOrderFormComponent } from './csv-order-form/csv-order-form.component';
+import { SharepointOrderFormComponent } from './sharepoint-order-form/sharepoint-order-form.component';
+import { CustomOrderFormComponent } from './custom-order-form/custom-order-form.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'form', component: RequestFormComponent,
+    children: [
+                {path: 'sql-order', component: SqlOrderFormComponent},
+                {path: 'csv-order', component: CsvOrderFormComponent},
+                {path: 'sharepoint-order', component: SharepointOrderFormComponent},
+                {path: 'custom-order', component: CustomOrderFormComponent}
+              ]
+  },
+  {path: 'orders', component: OrdersListComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [OrdersListComponent, RequestFormComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
 import { AppComponent } from './app.component';
+import { MainToolbarComponent } from './main-toolbar/main-toolbar.component';
+import { FormToolbarComponent } from './form-toolbar/form-toolbar.component';
+
+
+
+import { SqlOrderFormComponent } from './sql-order-form/sql-order-form.component';
+import { CsvOrderFormComponent } from './csv-order-form/csv-order-form.component';
+import { SharepointOrderFormComponent } from './sharepoint-order-form/sharepoint-order-form.component';
+import { CustomOrderFormComponent } from './custom-order-form/custom-order-form.component';
+import { BusinessPartFormComponent } from './business-part-form/business-part-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MDBBootstrapModule} from 'angular-bootstrap-md';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainToolbarComponent,
+    FormToolbarComponent,
+
+    routingComponents,
+    SqlOrderFormComponent,
+    CsvOrderFormComponent,
+    SharepointOrderFormComponent,
+    CustomOrderFormComponent,
+    BusinessPartFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
